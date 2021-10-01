a2c5c176924b4e9c298701d69cef56e19c644264
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-01 14:54:25 +0300

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-   const profileElement = document.querySelector('.image');
+   const profileElement = document.querySelector('.image>.profile');
    profileElement.addEventListener('click', (e) => {
      if(!(e.target.className).includes('.active')){
      e.target.className += '.active';
