cbbbafe0bccc8e7265c88e626146b6c848d6f4aa
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-01 13:28:14 +0300

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
+import { size } from "mathjs";
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+   const profileElement = document.querySelector('.profile');
+   profileElement.addEventListener('click', (e) => {
+     e.target.className += '.active';
+   });
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
