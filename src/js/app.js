601a8b157b54268215a5616bc5bbd35ea6740394
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-01 14:41:23 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,9 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
    const profileElement = document.querySelector('.image');
    profileElement.addEventListener('click', (e) => {
+     if(!(e.target.className).includes('.active')){
      e.target.className += '.active';
+     }
    });
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
