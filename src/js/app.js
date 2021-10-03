8b7b214066338735b3ea2c8338bc2eb4c40a56fd
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-03 13:25:18 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,13 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-   const profileElement = document.querySelector('.\\.image');
+   const profileElement = document.querySelector('.image');
    profileElement.addEventListener('click', (e) => {
-     if(!(e.target.className).includes('active')){
-     e.target.classList.add('active')
-    
+    //  if(!(e.target.className).includes('.active')){
+    //  e.target.className += '.active';
+    e.target.className = 'active';
      }
-   });
+   );
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
