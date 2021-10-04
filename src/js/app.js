fd724ae4dbbfdb5071aab665bd5255ce1d84f697
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-04 11:35:43 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,10 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
    const profileElement = document.querySelector('.image');
    profileElement.addEventListener('click', (e) => {
-    //  if(!(e.target.className).includes('.active')){
-    //  e.target.className += '.active';
-    e.target.className = 'active';
+     if(!(e.target.className).includes('.active')){
+     e.target.className += '.active';
      }
-   );
+    });
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
